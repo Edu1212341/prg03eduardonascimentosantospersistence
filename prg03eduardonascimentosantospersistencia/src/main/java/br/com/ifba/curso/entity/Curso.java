9ab9f2fb9b47849d7ddb750d6c105d732c5c8a33
Edu1212341 <167384147+Edu1212341@users.noreply.github.com>
2026-05-02 22:31:40 -0300

    feat: Atividade 1 com telas prontas

---
+++ prg03eduardonascimentosantospersistencia/src/main/java/br/com/ifba/curso/entity/Curso.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.curso.entity;
+
+/**
+ *
+ * @author eduardo
+ */
+
+public class Curso {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private int cargaHoraria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    
+}
